76f9f9302dedf36ca71619bea27ca31a05be7d6e
Ben Ong <ong.ben@gmail.com>
2025-03-15 00:01:34 -0700

    Added logic to log in user

--- utils/auth.js
+++ utils/auth.js
@@ -1,13 +1,23 @@
 import axios from 'axios';
 import { EXPO_PUBLIC_GOOGLE_WEB_API_KEY } from '@env';
 
-export async function createUser(email, password) {
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=` + EXPO_PUBLIC_GOOGLE_WEB_API_KEY;
   const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + EXPO_PUBLIC_GOOGLE_WEB_API_KEY,
+    url,
     {
       email: email,
       password: password,
       returnSecureToken: true
     }
   );
+  console.log(response.data);
+}
+
+export async function createUser(email, password) {
+  await authenticate('signUp', email, password);
+}
+
+export async function loginUser(email, password) {
+  await authenticate('signInWithPassword', email, password);
 }
