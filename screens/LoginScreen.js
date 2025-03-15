844a88ac2af195e4c39549de30b7851348e71131
Ben Ong <ong.ben@gmail.com>
2025-03-15 15:00:32 -0700

    Added error handling for login and signup screens

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,13 +2,21 @@ import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import { loginUser } from '../utils/auth';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { Alert } from 'react-native';
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
   
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
-    await loginUser(email, password);
+    try {
+      await loginUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        'Authentication failed!',
+        'Check your credentials or try again later!'
+      )
+    }
     setIsAuthenticating(false);
   }
 
