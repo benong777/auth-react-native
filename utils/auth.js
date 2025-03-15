6de566b3a90b40b11bf8c48f6ec9ff7938e4f0ae
Ben Ong <ong.ben@gmail.com>
2025-03-14 23:16:10 -0700

    Fixed API_KEY to .env file

--- utils/auth.js
+++ utils/auth.js
@@ -3,7 +3,6 @@ import { EXPO_PUBLIC_API_KEY } from '@env';
 
 export async function createUser(email, password) {
   const response = await axios.post(
-    // 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + process.env.EXPO_PUBLIC_API_KEY,
     'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + EXPO_PUBLIC_API_KEY,
     {
       email: email,
