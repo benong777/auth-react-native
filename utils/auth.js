80c49a665432739e7e1aedd93402ba4417b9e452
Ben Ong <ong.ben@gmail.com>
2025-03-14 23:20:48 -0700

    Renamed Google web API key from API_KEY to GOOGLE_WEB_API_KEY

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import { EXPO_PUBLIC_API_KEY } from '@env';
+import { EXPO_PUBLIC_GOOGLE_WEB_API_KEY } from '@env';
 
 export async function createUser(email, password) {
   const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + EXPO_PUBLIC_API_KEY,
+    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + EXPO_PUBLIC_GOOGLE_WEB_API_KEY,
     {
       email: email,
       password: password,
