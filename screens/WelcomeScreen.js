d537aea8df378622c3e82a620fdc1ef3a09e46c5
Ben Ong <ong.ben@gmail.com>
2025-03-18 00:32:43 -0700

    Added logic to allow access to Protected Resources

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
+import { useState, useEffect, useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  useEffect(() => {
+    axios.get(
+      'https://kast-41337-default-rtdb.firebaseio.com/message.json?auth=' + token
+      ).then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
